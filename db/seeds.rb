07bdfc1e76da2194048fad9ac81a4280bf1cd876
Masen Matthews <masenmatthews@gmail.com>
2018-05-14 14:00:00 -0700

    remove email column, add website column to organizations table

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,41 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+class Seed
+
+  def self.begin
+    seed = Seed.new
+    seed.generate_organizations
+  end
+
+  def generate_organizations
+    # Destination.destroy_all
+    # Review.destroy_all
+
+    20.times do |i|
+      Organization.create!(
+        category: Faker::Address.country,
+      )
+      @organization_id = Organization.last.id
+    end
+  end
+end
+
+p "Created #{Organization.count} organizations"
+Seed.begin
+
+organization_list = [
+  [ "Transition Projects", "Homelessness", "Transition Projects delivers life-saving and life-changing assistance to some of Portland’s most vulnerable residents. Whether by helping a homeless veteran and her family find housing, sheltering hundreds of people each night with nowhere else to turn, or opening new pathways to employment, Transition Projects represents an invaluable part of Portland’s social fabric.", "665 NW Hoyt St, Portland, OR 97209", "(503) 280-4700", "tamara@tprojects.org" ],
+  [ "Portland Homeless Family Solutions", "Homelessness", "", "1221 SW Yamhill St #210, Portland, OR 97205", "(503) 915-8306", "http://pdxhfs.org/" ],
+  [ "Sycamore", "Platanus" ],
+  [ "Alder", "Alnus" ],
+  [ "Birch", "Betula"],
+  [ "Cherry", "Prunus"]
+]
+
+organization_list.each do |name, category, description, address, phone, email|
+  Organization.create(
+    name: name,
+    category: category,
+    description: description,
+    address: address,
+    phone: phone,
+    email: email)
+end
