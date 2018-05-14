b3c8cfbc77f6e2d8cd646b7fe030ca299601b4aa
Masen Matthews <masenmatthews@gmail.com>
2018-05-14 13:31:05 -0700

    add organizations table

---
+++ db/migrate/20180514202629_add_organizations_table.rb
@@ -0,0 +1,14 @@
+class AddOrganizationsTable < ActiveRecord::Migration[5.2]
+  def change
+    create_table :organizations do |t|
+      t.column :name, :string
+      t.column :category, :string
+      t.column :description, :string
+      t.column :address, :string
+      t.column :phone, :string
+      t.column :email, :string
+
+      t.timestamps
+    end
+  end
+end
