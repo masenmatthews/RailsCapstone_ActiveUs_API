b7485ee0111ee793b70c10a7ec8dac0f4ac4602f
Masen Matthews <masenmatthews@gmail.com>
2018-05-16 15:53:41 -0700

    bug fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::API
-  # include Response
+  include Response
   # include ActionController::HttpAuthentication::Token::ControllerMethods
   # rescue_from ActiveRecord::RecordNotFound do |exception|
   #   json_response({ message: exception.message }, :not_found)
